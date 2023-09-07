2431ae6bffc112f84785791653c5d8f34d5030ea
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-09-07 14:45:54 -0300

    Meal Detail Instructions

--- app/src/main/java/com/example/foodapp/activities/MealActivity.kt
+++ app/src/main/java/com/example/foodapp/activities/MealActivity.kt
@@ -2,23 +2,46 @@ package com.example.foodapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.foodapp.R
 import com.example.foodapp.databinding.ActivityMealBinding
 import com.example.foodapp.fragments.HomeFragment
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.viewmodel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
     private lateinit var binding: ActivityMealBinding
+    private lateinit var mealMvvm:MealViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mealMvvm = ViewModelProviders.of(this)[MealViewModel::class.java]
+
         getMealInformationFromIntent()
         setInformationInViews()
+        mealMvvm.getMealDetail(mealId)
+        observerMealDetailsLiveData()
+    }
+
+    private fun observerMealDetailsLiveData() {
+        mealMvvm.observerMealDetailsLiveData().observe(this, object : Observer<Meal>{
+            override fun onChanged(value: Meal) {
+                val meal = value
+
+                binding.tvCategory.text = "Category: ${meal.strCategory}"
+                binding.tvArea.text = "Area: ${meal.strArea}"
+                binding.tvInstructionsSteps.text = meal.strInstructions
+            }
+
+        })
     }
 
     private fun setInformationInViews() {
