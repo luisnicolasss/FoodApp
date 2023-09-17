efad2d32175a0f0dd5b4ab8b218329a647a14b17
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-09-17 18:49:27 -0300

    Save UI state

--- app/src/main/java/com/example/foodapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/foodapp/viewmodel/HomeViewModel.kt
@@ -26,14 +26,20 @@ class HomeViewModel(val mealDatabase: MealDatabase) : ViewModel() {
     private var favoritesMealsLiveData = mealDatabase.mealDao().getAllMeals()
     private var bottomSheetMealLiveData = MutableLiveData<Meal>()
     private var searchedMealsLiveData = MutableLiveData<List<Meal>>()
-    fun getRandomMeal() {
 
+
+    private var saveStateRandomMeal: Meal? = null
+    fun getRandomMeal() {
+        saveStateRandomMeal?.let { randomMeal ->
+            randomMealLiveData.postValue(randomMeal)
+            return
+        }
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if (response.body() != null) {
                     val randomMeal: Meal = response.body()!!.meals[0]
                     randomMealLiveData.value = randomMeal
-
+                    saveStateRandomMeal = randomMeal
                 } else {
                     return
                 }
